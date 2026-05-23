1429571c457976e0d1b03810528ec7bc24c4b5d4
meskatov <glavnoevziznipivo6@gmail.com>
2026-05-23 21:52:53 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,26 @@
+TOKEN = "8861224766:AAFg6dg0BWbtpSSOjvDSfzS45X5DQqhTuJs"  # СРОЧНО ЗАМЕНИТЬ!
+TECH_ADMIN_ID = 8303714098  # ВСТАВИТЬ ID ТЕХ АДМИНА (@meskatov)
+OWNER_ID = 8557521484  # ВСТАВИТЬ ID ВЛАДЕЛЬЦА
+
+# ВЕРИФИКАЦИЯ ЧЕРЕЗ ТГК
+TGK_CHANNEL_ID = -1003925824221  # ID канала
+TGK_INVITE_LINK = "https://t.me/+w5VHICSM43I1MTFh"
+
+# Infinity API
+INFINITY_API_URL = "https://infinity-check.online/find.php"
+INFINITY_TOKEN = "R8fK2Lm9QWv3E7ZpD1yU4C6VtX5H0BJs"
+
+# DepSearch API
+DEPSEARCH_TOKEN = "WDTHx2vqZGE38gchBe7oAewzB9ZPNpxU"
+DEPSEARCH_BASE_URL = "https://api.depsearch.sbs/"
+
+# BigBase API
+BIGBASE_URL = "https://bigbase.top/api/search"
+BIGBASE_TOKEN = "BOqMzQ63vPTPKs7gfUDrJru62SX2JaqC"
+
+# LunoSearch API
+LUNOSEARCH_URL = "https://api.hyperionsearch.xyz/ishchi"
+LUNOSEARCH_TOKEN = "LunoSearch-293816ff-4e00-4eb3-a0d4-9c4efb77e17a"
+
+# Поисковые типы
+SEARCH_TYPES = ["📱 Телефон", "👤 ФИО", "📧 Email", "🔍 Username"]
